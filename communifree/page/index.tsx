912874dfb2a831f628f2a96a7b5fad68983e7c20
MMoallin <Mohamed.moallin@outlook.com>
2023-06-09 16:24:49 +0100

    Added more pages

--- communifree/page/index.tsx
+++ communifree/page/index.tsx
@@ -1,5 +1,6 @@
+import Next from "next";
 import Image from 'next/image'
-import styles from './page.module.css'
+// import styles from './page.module.css'
 
 export default function Home() {
   return (
---
+++ communifree/page/login.tsx
@@ -0,0 +1,3 @@
+import Next from "next";
+
+
