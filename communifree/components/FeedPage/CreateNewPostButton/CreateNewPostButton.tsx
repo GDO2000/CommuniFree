16290880810a9c974f3f8978e8c8c9c6605f954c
Rebecca <prebecca118@yahoo.co.uk>
2023-06-12 11:10:28 +0100

    create post button created

---
+++ communifree/components/FeedPage/CreateNewPostButton/CreateNewPostButton.tsx
@@ -0,0 +1,10 @@
+
+
+export default function CreatePostButton() {
+    return(
+    <div>
+        <h1>Hellooo</h1>
+        <button>Create Listing</button>
+    </div>
+    );
+}
--- communifree/pages/feed.tsx
+++ communifree/pages/feed.tsx
@@ -1,2 +1,11 @@
 import Next from "next";
+import CreatePostButton from "../components/FeedPage/CreateNewPostButton/CreateNewPostButton";
+
+
+export default function Feed(){
+
+    return(
+        <CreatePostButton/>
+    )
+}
 
