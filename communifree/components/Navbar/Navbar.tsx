67df88965d64bfb6fbd628e32a591e65a6b0a40c
Chaya <Chaya.B.Milstein@gmail.com>
2023-06-12 14:59:26 +0100

    navbar css file made

--- communifree/components/Navbar/Navbar.tsx
+++ communifree/components/Navbar/Navbar.tsx
@@ -4,8 +4,15 @@ import Image from "next/image"
 export default function Navbar(){
     return(
     <nav>
-        <Image src='/Logo.png' alt = "Communifree logo"/>
-        <Image src="/userprofile_image.png" alt = "User profile logo"/>
+        <ul>
+            <li>
+            <Image src='/Logo.png' alt = "Communifree logo" id="logo" width="80" height="80"/>
+            </li>
+            <li>
+            <Image src="/userprofile_image.png" alt = "User profile logo" id="profile-pic" width="80" height="80"/>
+            </li>
+            <li>Log in</li>
+        </ul>
     </nav>
-    )
+    );
 }
