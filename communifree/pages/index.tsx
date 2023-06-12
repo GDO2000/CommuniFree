f22979033f67669522ee227acd12c7ad709fd04f
FKXHX <94874029+FKXHX@users.noreply.github.com>
2023-06-12 15:50:31 +0000

    The Team helped connect the CSS file properly

--- communifree/pages/index.tsx
+++ communifree/pages/index.tsx
@@ -2,7 +2,7 @@ import Next from "next";
 import Image from 'next/image'
 import CreatePostButton from "../components/FeedPage/CreateNewPostButton/CreateNewPostButton";
 import Feed from "../components/FeedPage/Feed/Feed"
-// import styles from './page.module.css'
+import '../components/Navbar/Navbar.css'
 import Navbar from "../components/Navbar/Navbar";
 
 export default function Home() {
