9639002e62190be7553cfe5d9d10063f300e6948
MMoallin <Mohamed.moallin@outlook.com>
2023-06-13 11:45:48 +0100

    Login Page work

--- communifree/next.config.js
+++ communifree/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  // Other webpack configuration options...
+  entry: '../communifree/pages/signin/signin.tsx' , //Adjust the filename and extension based on your project
+  //Other configuration options...
+};
--- communifree/pages/feed.tsx
+++ communifree/pages/feed.tsx
@@ -9,9 +9,3 @@ export default function Feed(){
     )
 }
 
-export default function Feed(){
-    return(
-        <>
-        </>
-    )
-}
