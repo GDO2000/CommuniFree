f15db1778128ff852ec8ecfc4ff7f28a07a905ed
FKXHX <94874029+FKXHX@users.noreply.github.com>
2023-06-14 09:16:16 +0000

    crossOrigin removed

--- communifree/pages/index.tsx
+++ communifree/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   <>
     <head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600&display=swap" rel="stylesheet"/>
     </head>
     <>
