f716420e0bc5cc701ca677db8ce29cc73de25e5b
Rebecca <prebecca118@yahoo.co.uk>
2023-06-16 14:44:42 +0100

    textInput file deleted

--- communifree/components/FeedPage/SearchBar/TextInput/TextInput.tsx
+++
@@ -1,8 +0,0 @@
-import Next from 'next';
-import React from 'react';
-
-export default function TextInput (props:{handleChange:(e: {target: {value:string;}})=>void}) {
-    return (
-        <></>
-    );
-}
