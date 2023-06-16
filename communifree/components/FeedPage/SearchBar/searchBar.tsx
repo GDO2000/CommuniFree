cb3eb33c793d9d0470125f8d841d4454c9f12a9f
Rebecca <prebecca118@yahoo.co.uk>
2023-06-16 14:33:40 +0100

    deleted the searchbutton file and folder

--- communifree/components/FeedPage/SearchBar/searchBar.tsx
+++ communifree/components/FeedPage/SearchBar/searchBar.tsx
@@ -1,8 +1,6 @@
 import Next from 'next';
 import React from 'react';
 import {useState} from 'react';
-import TextInput from './TextInput/TextInput';
-import SearchButton from './SearchButton/SearchButton';
 import "./SearchBar.css"
 import Image from 'next/image';
 
