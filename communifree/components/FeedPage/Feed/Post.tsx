08d914ee9ec30c81c9f18c33e47c07adb1ad87c6
Rebecca <prebecca118@yahoo.co.uk>
2023-06-21 11:14:52 +0100

    posts are commected to Supabase sucessfully

--- communifree/components/FeedPage/Feed/Post.tsx
+++ communifree/components/FeedPage/Feed/Post.tsx
@@ -1,12 +1,12 @@
 import './Post.css'
 
-export default function Post () {
+export default function Post ({post}) {
     return (
         <div className="post">
         <img className='postImage' src='/carrots.png' alt='carrots'/>
-        <h1 className='postTitle'>Many Carrots</h1>
-        <h2 className='postLocation'>London</h2>
-        <p className='postDescription'>A lovely box of carrots I’ve just bought too many</p>
+        <h1 className='postTitle'>{post.title}</h1>
+        <h2 className='postLocation'>{post.location}</h2>
+        <p className='postDescription'>{post.description}</p>
         </div>
     )
 };
