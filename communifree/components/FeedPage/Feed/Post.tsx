89cb3a2aa9b1be925dbe8c43048a6ce5926b661f
Chaya <Chaya.B.Milstein@gmail.com>
2023-06-21 06:54:19 -0400

    images in the posts

--- communifree/components/FeedPage/Feed/Post.tsx
+++ communifree/components/FeedPage/Feed/Post.tsx
@@ -3,7 +3,7 @@ import './Post.css'
 export default function Post ({post}) {
     return (
         <div className="post">
-        <img className='postImage' src='/carrots.png' alt='carrots'/>
+        <img className='postImage' src={post.image} alt='carrots'/>
         <h1 className='postTitle'>{post.title}</h1>
         <h2 className='postLocation'>{post.location}</h2>
         <p className='postDescription'>{post.description}</p>
