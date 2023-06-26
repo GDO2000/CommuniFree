9277c4685d4b8294d3ec9339ecf4936b8bfba3ea
Chaya <Chaya.B.Milstein@gmail.com>
2023-06-26 07:17:22 -0400

    cropped images and better grid responsiveness

--- communifree/components/FeedPage/Feed/Post.tsx
+++ communifree/components/FeedPage/Feed/Post.tsx
@@ -3,12 +3,12 @@ import './Post.css'
 export default function Post ({post}) {
     return (
         <div className="post">
-        <div className="img-container">
-        <img className='postImage' src={post.image} alt='Picture of Item'/>
+            <div className="img-container">
+                <img className='postImage' src={post.image} alt='Picture of Item'/>
+            </div>
+            <h1 className='postTitle'>{post.title}</h1>
+            <h2 className='postLocation'>{post.location}</h2>
+            <p className='postDescription'>{post.description}</p>
         </div>
-        <h1 className='postTitle'>{post.title}</h1>
-        <h2 className='postLocation'>{post.location}</h2>
-        <p className='postDescription'>{post.description}</p>
-        </div>
-    )
-};
+    );
+}
