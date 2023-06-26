075823312e6b78e7dfe3f6e0961e19235b5e64d9
FKXHX <94874029+FKXHX@users.noreply.github.com>
2023-06-26 10:22:43 +0000

    Size changes

--- communifree/components/FeedPage/Feed/Post.tsx
+++ communifree/components/FeedPage/Feed/Post.tsx
@@ -3,7 +3,9 @@ import './Post.css'
 export default function Post ({post}) {
     return (
         <div className="post">
+        <div className="img-container">
         <img className='postImage' src={post.image} alt='Picture of Item'/>
+        </div>
         <h1 className='postTitle'>{post.title}</h1>
         <h2 className='postLocation'>{post.location}</h2>
         <p className='postDescription'>{post.description}</p>
