dd3ac76a1aab0f07520636d00fbebfad387b12b9
MMoallin <Mohamed.moallin@outlook.com>
2023-06-26 12:04:06 +0100

    Styling of Homepage/signin page

--- communifree/pages/signin.tsx
+++ communifree/pages/signin.tsx
@@ -6,6 +6,7 @@ import {  ThemeSupa } from '@supabase/auth-ui-shared'
 import { useRouter } from 'next/router'
 import { useSession} from '../backend/db/session'
 import  './signin.css'
+import Image from "next/image"
 
 
 const supabase = createClient('https://ukdeopjzktiqoppsbbvq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrZGVvcGp6a3RpcW9wcHNiYnZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYwNDE5MzAsImV4cCI6MjAwMTYxNzkzMH0.UiwFY43g8klf3t182m4kUoDlsVkci-US1gEq-INk8vk')
@@ -19,10 +20,28 @@ export default function SignIn() {
 
   if (!session) {
     return (
-  <div id="Auth">
-  <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
-  </div>
+             <div id = 'signIn'>
+                 <div id = 'header'> 
+
+                 <Image src='/Logo.png' alt = "Communifree logo" id="logo" width="150" height="80"/>
+                 
+                 </div>
+                 <div id="Auth">
+                    <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+                 </div>
+                 <div id = 'footer'></div>
+             </div>
+                            
+  
+  
+
+
+
+
+
+
   )} else {
     router.push('/');
   }
-}
+  }
+
