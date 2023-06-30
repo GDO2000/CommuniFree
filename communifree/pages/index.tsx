a4f4817ddb1aab0a4dc61bbf8bf1e048d2476da6
GDO2000 <56234863+GDO2000@users.noreply.github.com>
2023-06-30 09:54:28 +0100

    Update index.tsx

--- communifree/pages/index.tsx
+++ communifree/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-// import supabase from '../../communifree/utils/supabaseClient'
+import supabase from '../../communifree/utils/supabaseClient'
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import {  ThemeSupa } from '@supabase/auth-ui-shared'
@@ -10,8 +10,6 @@ import  './index.css'
 import Image from "next/image"
 import Head from 'next/head';
 
-
-const supabase = createClient('https://ukdeopjzktiqoppsbbvq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrZGVvcGp6a3RpcW9wcHNiYnZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYwNDE5MzAsImV4cCI6MjAwMTYxNzkzMH0.UiwFY43g8klf3t182m4kUoDlsVkci-US1gEq-INk8vk')
 
 
 export default function SignIn() {
